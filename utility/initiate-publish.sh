6626b0011f88d15cb35597ee04c6ab016c1217b8
davidascher <david.ascher@gmail.com>
2014-06-23 13:28:55 -0700

    trying

--- utility/initiate-publish.sh
+++ utility/initiate-publish.sh
@@ -5,13 +5,13 @@ echo -e "Publishing website...\n"
 #cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/mozilla/mobile-opportunity gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/mozillafordevelopment/mobile-opportunity gh-pages > /dev/null
 
 cd gh-pages
 git rm -rf .
 cp -Rf ../_built_website/* .
 git add -f .
 git commit -m "Lastest harp compiled site on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-git push -fq origin gh-pages > /dev/null
+git push -fq origin https://${GH_TOKEN}@github.com/mozillafordevelopment/mozillafordevelopment.github.io master > /dev/null
 
-echo -e "Published website to gh-pages.\n"
+echo -e "Published website to master branch of pages repo.\n"
