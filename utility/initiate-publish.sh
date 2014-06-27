f4da1c51bd812daae9b059dbbe5df5a157ad1494
travis-ci <travis@travis-ci.org>
2014-06-27 15:40:42 -0700

    branch names

--- utility/initiate-publish.sh
+++ utility/initiate-publish.sh
@@ -5,13 +5,13 @@ echo -e "Publishing website...\n"
 #cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/mozillafordevelopment/mozillafordevelopment.github.io gh-pages > /dev/null
+git clone --quiet --branch=website https://${GH_TOKEN}@github.com/mozillafordevelopment/mozillafordevelopment.github.io website > /dev/null
 
-cd gh-pages
+cd website
 git rm -rf .
 cp -Rf ../_built_website/* .
 git add -f .
 git commit -m "Lastest harp compiled site on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin master > /dev/null
 
-echo -e "Published website to master branch of pages repo.\n"
+echo -e "Published website to master.\n"
