79d93dff3d718c2c86554effd43f1f1526858de4
travis-ci <travis@travis-ci.org>
2014-06-27 15:53:12 -0700

    typo

--- utility/initiate-publish.sh
+++ utility/initiate-publish.sh
@@ -7,7 +7,7 @@ git clone --quiet --branch=website https://${GH_TOKEN}@github.com/mozillafordeve
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://${TOKEN}:@github.com" > .git/credentials
-got config user.name "davidascher"
+git config user.name "davidascher"
 
 cd website
 git rm -rf .
