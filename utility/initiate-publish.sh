25ac22cbce8c3f00e2ac8c875b0a15c60e20f762
travis-ci <travis@travis-ci.org>
2014-06-27 16:21:36 -0700

    straws again

--- utility/initiate-publish.sh
+++ utility/initiate-publish.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-#cd $HOME
 echo -e "Getting branch...\n"
-git clone --quiet --branch=website https://${GH_TOKEN}@github.com/mozillafordevelopment/mozillafordevelopment.github.io website > /dev/null
+git clone --quiet --branch=website https://${TOKEN}@github.com/mozillafordevelopment/mozillafordevelopment.github.io website > /dev/null
 
 cd website
 git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+echo "https://${TOKEN}:@github.com" > .git/credentials
 echo "CREDS"
 cat .git/credentials
 git config --global user.name "davidascher"
