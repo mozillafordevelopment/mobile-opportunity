c86ad448d4b47d2813357269f02ee95996e97944
travis-ci <travis@travis-ci.org>
2014-06-27 16:16:39 -0700

    foo

--- utility/initiate-publish.sh
+++ utility/initiate-publish.sh
@@ -7,7 +7,7 @@ git clone --quiet --branch=website https://${GH_TOKEN}@github.com/mozillafordeve
 
 cd website
 git config credential.helper "store --file=.git/credentials"
-echo "https://${TOKEN}:@github.com" > .git/credentials
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 echo "CREDS"
 cat .git/credentials
 git config --global user.name "davidascher"
