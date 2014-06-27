fcff0445feff5dc2d6ce3ad2fabd9968b088377b
travis-ci <travis@travis-ci.org>
2014-06-27 15:51:34 -0700

    trying a different approach to auth

--- utility/initiate-publish.sh
+++ utility/initiate-publish.sh
@@ -3,15 +3,17 @@
 echo -e "Publishing website...\n"
 
 #cd $HOME
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
 git clone --quiet --branch=website https://${GH_TOKEN}@github.com/mozillafordevelopment/mozillafordevelopment.github.io website > /dev/null
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${TOKEN}:@github.com" > .git/credentials
+got config user.name "davidascher"
 
 cd website
 git rm -rf .
 cp -Rf ../_built_website/* .
 git add -f .
 git commit -m "Lastest harp compiled site on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-git push -fq origin master > /dev/null
+git push origin master > /dev/null
 
 echo -e "Published website to master.\n"
